b5bbebfb421a4250180913a962b56088abd33299
to dong luu <todongluu@outlook.com>
2018-10-30 20:03:09 +0700

    first commit

---
+++ NMCNPM_PMChoThueXe/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NMCNPM_PMChoThueXe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string username = txtTenTaiKhoan.Text;
+            string password = txtMatKhau.Text;
+            if (username == "dongluu" && password == "nhom7")
+            {
+                this.DialogResult = System.Windows.Forms.DialogResult.OK;
+
+            }
+            else
+            {
+                // neu dang nhap dong dung thi xoa txt de nhap lai
+                txtTenTaiKhoan.Clear();
+                txtMatKhau.Clear();
+            }
+        }
+    }
+}
---
+++ NMCNPM_PMChoThueXe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NMCNPM_PMChoThueXe
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            if(new Form1().ShowDialog()==DialogResult.OK)
+            {
+                Application.Run(new Trangchu());
+            }
+            Application.Run(new Form1());
+        }
+    }
+}
